38ada5030dbc23cc8a9dd279ab95843fd59c959b
Becky Auger-Williams <rjw@observatorysciences.co.uk>
2024-11-15 08:53:49 +0000

    Test

--- docs/source/_static/javascript/readthedocs.js
+++ docs/source/_static/javascript/readthedocs.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 // Trigger Read the Docs' search addon instead of Material MkDocs default
-document.querySelector(".md-search__input").addEventListener("focus", (e) => {
+document.querySelector("btn search-button-field search-button__button pst-js-only").addEventListener("click", (e) => {
         const event = new CustomEvent("readthedocs-search-show");
         document.dispatchEvent(event);
         console.log("here");
