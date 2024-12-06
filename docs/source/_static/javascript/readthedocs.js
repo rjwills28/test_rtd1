32213a4d10c39509909868e34b971c1076232088
Becky Auger-Williams <rjw@observatorysciences.co.uk>
2024-12-06 08:29:33 +0000

    test css

--- docs/source/_static/javascript/readthedocs.js
+++ docs/source/_static/javascript/readthedocs.js
@@ -5,7 +5,31 @@ original_search.parentNode.replaceChild(new_search, original_search);
 new_search.addEventListener("click", (e) => {
         const event = new CustomEvent("readthedocs-search-show");
         document.dispatchEvent(event);
-        document.querySelector("div.content form input").style.color="black";
+        //document.querySelector("div.content form input").style.color="black";
+        waitForElm('div.content form input').then((elm) => {
+            console.log('Element is ready');
+            console.log(elm.textContent);
+        });
     });
 });
 
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
