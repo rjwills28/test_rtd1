13b0485db76c32c7ba924345b6186b1370dbd0e2
Becky Auger-Williams <rjw@observatorysciences.co.uk>
2024-12-06 11:02:12 +0000

    test css

--- docs/source/_static/javascript/readthedocs.js
+++ docs/source/_static/javascript/readthedocs.js
@@ -4,28 +4,7 @@ var new_search = original_search.cloneNode(true);
 original_search.parentNode.replaceChild(new_search, original_search);
 new_search.addEventListener("click", (e) => {
         const event = new CustomEvent("readthedocs-search-show");
-        console.log("Dispatching event");
-        //document.querySelector("div.content form input").style.color="black";
-        waitForElement('div form input', (element) => {
-            console.log('Element is ready');
-            console.log(element.style);
-            element.style.color="black";
-        });
         document.dispatchEvent(event);
-        console.log("Wait for elm completed");
+        document.querySelector("readthedocs-search").shadowRoot.querySelector("div form input").style.color="black";
     });
 });
-
-function waitForElement(selector, callback) {
-    
-    const interval = setInterval(() => {
-        console.log("Starting");
-        const element = document.querySelector(selector);
-        if (element) {
-            clearInterval(interval);
-            callback(element);
-            console.log("Found");
-        }
-        console.log("Didn't find");
-    }, 100); // Check every 100ms
-}
